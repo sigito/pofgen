b2d53c00514ba2784a1dc80f8af176a1e67bf1fb
Yuri Samsoniuk <yuri.samsoniuk@gmail.com>
2013-05-09 13:19:01 +0300

    Initial commit.

---
+++ src/org/idea/plugin/pofgen/GeneratePofAction.java
@@ -0,0 +1,13 @@
+package org.idea.plugin.pofgen;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author sigito
+ */
+public class GeneratePofAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
