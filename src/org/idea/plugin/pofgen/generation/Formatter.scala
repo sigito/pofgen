f05984a2e0f3a2d7e5f39557c7df1f4bfa8a1cbf
Yuri Samsoniuk <yuri.samsoniuk@gmail.com>
2014-01-17 23:34:24 +0200

    Added imports optimization while formatting.

--- src/org/idea/plugin/pofgen/generation/Formatter.scala
+++ src/org/idea/plugin/pofgen/generation/Formatter.scala
@@ -10,6 +10,7 @@ object Formatter {
   def format(element: PsiElement, context: GenerationContext) = {
     // process code formatting
     JavaCodeStyleManager.getInstance(context.project).shortenClassReferences(element)
+    JavaCodeStyleManager.getInstance(context.project).optimizeImports(element.getContainingFile)
     CodeStyleManager.getInstance(context.project).reformat(element)
   }
 }
